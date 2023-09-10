a578198ffaa9bb6e8ecdd363579aad1120105343
Podskio <podskio1@gmail.com>
2023-09-10 13:35:17 -0700

    Extend backgroundImage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,10 +18,10 @@ module.exports = {
         ["red-glow"]: ["2px 2px 10px #f00505"],
         ["blue-glow"]: ["2px 2px 10px #0598f6"],
       },
-    },
-    backgroundImage: {
-      ["gradient-brand"]:
-        "linear-gradient(92.91deg, #0598F4 -50%,#F00605 150%)",
+      backgroundImage: {
+        ["gradient-brand"]:
+          "linear-gradient(92.91deg, #0598F4 -50%,#F00605 150%)",
+      },
     },
   },
   plugins: [],
