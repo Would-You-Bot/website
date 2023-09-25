e7bff1c2d79947224fa11e12ebb347ac06d70d56
Podskio <podskio1@gmail.com>
2023-09-24 22:32:07 -0700

    Styling adjustments

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,20 @@
 import { VariantProps, cva } from "class-variance-authority";
 
-const button = cva("flex items-center rounded-lg px-6 py-3 transition-all", {
-  variants: {
-    variant: {
-      neutral: "bg-neutral-800 hover:bg-neutral-700 text-neutral-300",
-      blue: "bg-brand-blue-100 hover:bg-brand-blue-200 text-white",
-      red: "bg-brand-red-100 hover:bg-brand-red-200 text-white",
+const button = cva(
+  "flex items-center rounded-lg px-6 py-3 transition-all disabled:cursor-not-allowed disabled:brightness-75 text-neutral-400",
+  {
+    variants: {
+      variant: {
+        neutral: "bg-neutral-800 hover:bg-neutral-700 text-neutral-300",
+        blue: "bg-brand-blue-100 hover:bg-brand-blue-200 text-white",
+        red: "bg-brand-red-100 hover:bg-brand-red-200 text-white",
+      },
+    },
+    defaultVariants: {
+      variant: "blue",
     },
   },
-  defaultVariants: {
-    variant: "blue",
-  },
-});
+);
 
 interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
