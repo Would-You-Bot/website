9c22ba53c132cfc6389499b46437853e90865fe9
Podskio <podskio1@gmail.com>
2023-10-09 12:43:04 -0700

    Redirect on signin

--- server/auth.ts
+++ server/auth.ts
@@ -45,6 +45,9 @@ export const authOptions: NextAuthOptions = {
       }
       return token;
     },
+    signIn: () => {
+      return "/guilds";
+    },
   },
   providers: [
     DiscordProvider({
