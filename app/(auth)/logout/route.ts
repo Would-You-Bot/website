363333a25478539cfca30fff5985a2c723212229
Diogo Paulos <itzframepvp@outlook.com>
2024-12-30 23:35:13 +0000

    fix: attempting to fix unsafe cookies

--- app/(auth)/logout/route.ts
+++ app/(auth)/logout/route.ts
@@ -1,26 +1,28 @@
-import { getAuthTokenOrNull } from '@/helpers/oauth/helpers'
-import { discordOAuthClient } from '@/helpers/oauth'
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
+import { getAuthTokenOrNull } from "@/helpers/oauth/helpers";
+import { discordOAuthClient } from "@/helpers/oauth";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export async function GET() {
-  const token = await getAuthTokenOrNull()
+export async function GET(req: NextRequest) {
+	const cookieStore = await cookies();
+	const token = await getAuthTokenOrNull();
+	const redirectUrl = req.nextUrl.searchParams.get("redirect");
 
-  if (token) {
-    await Promise.allSettled([
-      discordOAuthClient.revokeToken(
-        token.payload.discord.access_token,
-        'access_token'
-      ),
-      discordOAuthClient.revokeToken(
-        token.payload.discord.refresh_token,
-        'refresh_token'
-      )
-    ])
-  }
+	if (token) {
+		await Promise.allSettled([
+			discordOAuthClient.revokeToken(
+				token.payload.discord.access_token,
+				"access_token",
+			),
+			discordOAuthClient.revokeToken(
+				token.payload.discord.refresh_token,
+				"refresh_token",
+			),
+		]);
+	}
 
-  cookies().delete('OAUTH_TOKEN')
-  cookies().delete('ID_TOKEN')
+	cookieStore.delete("OAUTH_TOKEN");
+	cookieStore.delete("ID_TOKEN");
 
-  return redirect('/')
+	return redirect("/");
 }
--- helpers/oauth/helpers.ts
+++ helpers/oauth/helpers.ts
@@ -1,13 +1,13 @@
-import { OAuthTokenJWT } from '@/helpers/oauth/types'
-import { IdTokenJWT } from '@/helpers/hooks'
-import { verifyJwt } from '@/helpers/jwt'
-import { cookies } from 'next/headers'
-import { parseJWT } from 'oslo/jwt'
-import { cache } from 'react'
+import type { OAuthTokenJWT } from '@/helpers/oauth/types';
+import type { IdTokenJWT } from '@/helpers/hooks';
+import { verifyJwt } from '@/helpers/jwt';
+import { cookies } from 'next/headers';
+import { parseJWT } from '@oslojs/jwt';
 
-export function getIdToken(): IdTokenJWT | null {
-  const tokenString = cookies().get('ID_TOKEN')?.value
-  return tokenString ? (parseJWT(tokenString) as IdTokenJWT) : null
+export async function getIdToken(inputToken?: string): Promise<IdTokenJWT | null> {
+    const cookieStore = await cookies();
+    const tokenString = inputToken || cookieStore.get('ID_TOKEN')?.value;
+    return tokenString ? (parseJWT(tokenString) as IdTokenJWT) : null;
 }
 
 /**
@@ -15,26 +15,28 @@ export function getIdToken(): IdTokenJWT | null {
  * including the signature, expiration, and not-before date.
  * Throws if the JWT is invalid or expired.
  */
-export function getAuthToken(): Promise<OAuthTokenJWT> {
-  const token = cookies().get('OAUTH_TOKEN')?.value
-
-  if (!token) {
-    throw new MissingTokenException()
-  }
-
-  return verifyJwt(token) as Promise<OAuthTokenJWT>
+export async function getAuthToken(inputToken?: string): Promise<OAuthTokenJWT> {
+    const cookieStore = await cookies();
+    const token = inputToken || cookieStore.get('OAUTH_TOKEN')?.value;
+    if (!token) {
+        throw new MissingTokenException();
+    }
+    return verifyJwt(token) as Promise<OAuthTokenJWT>;
 }
 
 /**
  * @desc Returns the validated auth token, or null if any error is thrown (e.g. not token found, token is expired, invalid signature,...)
  */
-export const getAuthTokenOrNull = cache((): Promise<OAuthTokenJWT | null> => {
-  try {
-    return getAuthToken()
-  } catch (e: unknown) {
-    return Promise.resolve(null)
-  }
-})
+export const getAuthTokenOrNull = async (
+    inputToken?: string
+): Promise<OAuthTokenJWT | null> => {
+    try {
+        return await getAuthToken(inputToken);
+    } catch (e) {
+        console.error('Error getting auth token:', e);
+        return null;
+    }
+};
 
 export abstract class OAuthException extends Error {}
 
