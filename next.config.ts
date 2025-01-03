16fa7e6d5412947967ccefe2a1aae76db5914f7e
Dominik Koch <dominik@koch-bautechnik.de>
2025-01-03 16:53:06 +0100

    switch to typescript nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   typescript: {
@@ -7,7 +8,6 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedForwardedHosts: ['wouldyoubot.gg'],
       allowedOrigins: ['wouldyoubot.gg']
     }
   },
