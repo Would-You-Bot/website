1dd490a2a5adfa697562ca0c1311b721a4ad5584
mezotv <mezotv@users.noreply.github.com>
2025-01-07 18:37:30 +0000

    chore(style): format files

--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -2,34 +2,34 @@ import Content from '@/components/Premium/Content'
 import type { Metadata, Viewport } from 'next'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://wouldyoubot.gg'),
-  title: 'Premium - Would You Bot',
-  description: 'Get premium for Would You Bot and unlock all features!',
-  twitter: {
-    title: 'Premium - Would You Bot',
-    description: 'Get premium for Would You Bot and unlock all features!'
-  },
-  openGraph: {
-    title: 'Premium - Would You Bot',
-    description: 'Get premium for Would You Bot and unlock all features!'
-  },
-  robots: {
-    index: true,
-    follow: true
-  }
+	metadataBase: new URL('https://wouldyoubot.gg'),
+	title: 'Premium - Would You Bot',
+	description: 'Get premium for Would You Bot and unlock all features!',
+	twitter: {
+		title: 'Premium - Would You Bot',
+		description: 'Get premium for Would You Bot and unlock all features!'
+	},
+	openGraph: {
+		title: 'Premium - Would You Bot',
+		description: 'Get premium for Would You Bot and unlock all features!'
+	},
+	robots: {
+		index: true,
+		follow: true
+	}
 }
 
 export const viewport: Viewport = {
-  themeColor: '#facc15',
-  maximumScale: 5
+	themeColor: '#facc15',
+	maximumScale: 5
 }
 
 export default function Premium() {
-  return (
-    <>
-      <main className="relative mb-40 flex w-full justify-center">
-        <Content />
-      </main>
-    </>
-  )
+	return (
+		<>
+			<main className="relative mb-40 flex w-full justify-center">
+				<Content />
+			</main>
+		</>
+	)
 }
--- helpers/hooks/useCookies.ts
+++ helpers/hooks/useCookies.ts
@@ -5,55 +5,55 @@ import { useState, useEffect } from 'react'
 import { useMemo } from 'react'
 
 export function useCookies() {
-  const isClient = useIsClient()
-  const cookies_ = isClient ? window?.document?.cookie : ''
+	const isClient = useIsClient()
+	const cookies_ = isClient ? window?.document?.cookie : ''
 
-  return useMemo(() => {
-    return cookies_.split(';').reduce((acc, cookie) => {
-      const [key, value] = cookie.split('=')
-      acc.set(key.trim(), value)
-      return acc
-    }, new Map<string, string>())
-  }, [cookies_])
+	return useMemo(() => {
+		return cookies_.split(';').reduce((acc, cookie) => {
+			const [key, value] = cookie.split('=')
+			acc.set(key.trim(), value)
+			return acc
+		}, new Map<string, string>())
+	}, [cookies_])
 }
 
 export interface CookiePreferences {
-  necessary: boolean
-  analytics: boolean
+	necessary: boolean
+	analytics: boolean
 }
 
 export const useCookiePreferences = () => {
-  const cookies = useCookies()
-  const [data, setData] = useState<CookiePreferences>({
-    necessary: true,
-    analytics: true
-  })
-  const [showBanner, setShowBanner] = useState(true)
+	const cookies = useCookies()
+	const [data, setData] = useState<CookiePreferences>({
+		necessary: true,
+		analytics: true
+	})
+	const [showBanner, setShowBanner] = useState(true)
 
-  useEffect(() => {
-    const savedPreferences = cookies.get('cookiePreferences')
-    if (savedPreferences) {
-      setData(JSON.parse(savedPreferences))
-      setShowBanner(false)
-    }
-  }, [cookies])
+	useEffect(() => {
+		const savedPreferences = cookies.get('cookiePreferences')
+		if (savedPreferences) {
+			setData(JSON.parse(savedPreferences))
+			setShowBanner(false)
+		}
+	}, [cookies])
 
-  const savePreferences = (preferences: typeof data) => {
-    setData(preferences)
-    const expires = new Date(Date.now() + 31 * 864e5).toUTCString()
-    document.cookie = `cookiePreferences=${JSON.stringify(preferences)}; expires=${expires}; path=/`
-  }
+	const savePreferences = (preferences: typeof data) => {
+		setData(preferences)
+		const expires = new Date(Date.now() + 31 * 864e5).toUTCString()
+		document.cookie = `cookiePreferences=${JSON.stringify(preferences)}; expires=${expires}; path=/`
+	}
 
-  const hasAnalytics = () => {
-    return data.analytics
-  }
+	const hasAnalytics = () => {
+		return data.analytics
+	}
 
-  return {
-    data,
-    setData,
-    showBanner,
-    setShowBanner,
-    savePreferences,
-    hasAnalytics
-  }
+	return {
+		data,
+		setData,
+		showBanner,
+		setShowBanner,
+		savePreferences,
+		hasAnalytics
+	}
 }
