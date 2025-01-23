929635378651df98ef0a722919527eb81ef0457f
mezotv <mezotv@users.noreply.github.com>
2025-01-23 01:05:20 +0000

    chore(style): format files

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,211 +5,211 @@ import { prisma } from '@/lib/prisma'
 import type Stripe from 'stripe'
 
 export async function POST(request: NextRequest) {
-  try {
-    const payload = await request.text()
-    const signature = request.headers.get('stripe-signature')
+	try {
+		const payload = await request.text()
+		const signature = request.headers.get('stripe-signature')
 
-    let event
-    try {
-      event = stripe.webhooks.constructEvent(
-        payload,
-        signature!,
-        process.env.STRIPE_WEBHOOK_SECRET!
-      )
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(`Webhook signature verification failed: ${error.message}`)
-      } else {
-        console.error(
-          'Webhook signature verification failed with unknown error type.'
-        )
-      }
-      return NextResponse.json(
-        { message: 'Webhook Error', status: 400 },
-        { status: 400 }
-      )
-    }
+		let event
+		try {
+			event = stripe.webhooks.constructEvent(
+				payload,
+				signature!,
+				process.env.STRIPE_WEBHOOK_SECRET!
+			)
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				console.error(`Webhook signature verification failed: ${error.message}`)
+			} else {
+				console.error(
+					'Webhook signature verification failed with unknown error type.'
+				)
+			}
+			return NextResponse.json(
+				{ message: 'Webhook Error', status: 400 },
+				{ status: 400 }
+			)
+		}
 
-    switch (event.type) {
-      // in the event of a successful checkout
-      case 'customer.subscription.created':
-        const subscriptionData: Stripe.Subscription = event.data.object
-        const serverId = subscriptionData.metadata?.serverId
+		switch (event.type) {
+			// in the event of a successful checkout
+			case 'customer.subscription.created':
+				const subscriptionData: Stripe.Subscription = event.data.object
+				const serverId = subscriptionData.metadata?.serverId
 
-        if (!serverId) {
-          return NextResponse.json(
-            { message: 'One or more variables are missing', status: 400 },
-            { status: 400 }
-          )
-        }
+				if (!serverId) {
+					return NextResponse.json(
+						{ message: 'One or more variables are missing', status: 400 },
+						{ status: 400 }
+					)
+				}
 
-        const subscription = await stripe.subscriptions.search({
-          query: `metadata['serverId']:'${serverId}'`
-        })
+				const subscription = await stripe.subscriptions.search({
+					query: `metadata['serverId']:'${serverId}'`
+				})
 
-        try {
-          await prisma.guild.upsert({
-            where: {
-              guildID: serverId
-            },
-            create: {
-              guildID: serverId,
-              premiumUser: subscription?.data[0]?.metadata.userId,
-              premium: 1,
-              premiumExpiration: new Date(
-                subscription.data[0].current_period_end * 1000
-              ),
-              language: 'en_US'
-            },
-            update: {
-              premiumUser: subscription?.data[0]?.metadata.userId,
-              premium: 1,
-              premiumExpiration: new Date(
-                subscription.data[0].current_period_end * 1000
-              )
-            }
-          })
-        } catch (error) {
-          console.error(error)
-          return NextResponse.json(
-            {
-              message: 'An error occurred while updating the database.',
-              status: 500
-            },
-            { status: 500 }
-          )
-        }
-        break
+				try {
+					await prisma.guild.upsert({
+						where: {
+							guildID: serverId
+						},
+						create: {
+							guildID: serverId,
+							premiumUser: subscription?.data[0]?.metadata.userId,
+							premium: 1,
+							premiumExpiration: new Date(
+								subscription.data[0].current_period_end * 1000
+							),
+							language: 'en_US'
+						},
+						update: {
+							premiumUser: subscription?.data[0]?.metadata.userId,
+							premium: 1,
+							premiumExpiration: new Date(
+								subscription.data[0].current_period_end * 1000
+							)
+						}
+					})
+				} catch (error) {
+					console.error(error)
+					return NextResponse.json(
+						{
+							message: 'An error occurred while updating the database.',
+							status: 500
+						},
+						{ status: 500 }
+					)
+				}
+				break
 
-      case 'invoice.paid':
-      case 'invoice.payment_succeeded':
-        const invoice: Stripe.Invoice = event.data.object
+			case 'invoice.paid':
+			case 'invoice.payment_succeeded':
+				const invoice: Stripe.Invoice = event.data.object
 
-        if (invoice.subscription_details === null)
-          return NextResponse.json(
-            { message: 'No subscription details found', status: 400 },
-            { status: 400 }
-          )
+				if (invoice.subscription_details === null)
+					return NextResponse.json(
+						{ message: 'No subscription details found', status: 400 },
+						{ status: 400 }
+					)
 
-        const serverIdInvoice = invoice.subscription_details.metadata?.serverId
+				const serverIdInvoice = invoice.subscription_details.metadata?.serverId
 
-        if (!serverIdInvoice) {
-          return NextResponse.json(
-            { message: 'One or more variables are missing', status: 400 },
-            { status: 400 }
-          )
-        }
+				if (!serverIdInvoice) {
+					return NextResponse.json(
+						{ message: 'One or more variables are missing', status: 400 },
+						{ status: 400 }
+					)
+				}
 
-        const subscriptionInvoice = await stripe.subscriptions.search({
-          query: `metadata['serverId']:'${serverIdInvoice}'`
-        })
-        try {
-          await prisma.guild.update({
-            where: {
-              guildID: serverIdInvoice
-            },
-            data: {
-              pending: false,
-              premiumExpiration: new Date(
-                subscriptionInvoice.data[0].current_period_end * 1000
-              )
-            }
-          })
-        } catch (error) {
-          console.error(error)
-          return NextResponse.json(
-            {
-              message: 'An error occurred while updating the database.',
-              status: 500
-            },
-            { status: 500 }
-          )
-        }
-        break
+				const subscriptionInvoice = await stripe.subscriptions.search({
+					query: `metadata['serverId']:'${serverIdInvoice}'`
+				})
+				try {
+					await prisma.guild.update({
+						where: {
+							guildID: serverIdInvoice
+						},
+						data: {
+							pending: false,
+							premiumExpiration: new Date(
+								subscriptionInvoice.data[0].current_period_end * 1000
+							)
+						}
+					})
+				} catch (error) {
+					console.error(error)
+					return NextResponse.json(
+						{
+							message: 'An error occurred while updating the database.',
+							status: 500
+						},
+						{ status: 500 }
+					)
+				}
+				break
 
-      // in the event of a subscription being updated
-      case 'customer.subscription.updated':
-        const subscriptionDataUpdated: Stripe.Subscription = event.data.object
-        const serverIdUpdated = subscriptionDataUpdated.metadata?.serverId
+			// in the event of a subscription being updated
+			case 'customer.subscription.updated':
+				const subscriptionDataUpdated: Stripe.Subscription = event.data.object
+				const serverIdUpdated = subscriptionDataUpdated.metadata?.serverId
 
-        if (!serverIdUpdated) {
-          return NextResponse.json(
-            { message: 'One or more variables are missing', status: 400 },
-            { status: 400 }
-          )
-        }
+				if (!serverIdUpdated) {
+					return NextResponse.json(
+						{ message: 'One or more variables are missing', status: 400 },
+						{ status: 400 }
+					)
+				}
 
-        const subscriptionUpdated = await stripe.subscriptions.search({
-          query: `metadata['serverId']:'${serverIdUpdated}'`
-        })
+				const subscriptionUpdated = await stripe.subscriptions.search({
+					query: `metadata['serverId']:'${serverIdUpdated}'`
+				})
 
-        await prisma.guild.update({
-          where: {
-            guildID: serverIdUpdated
-          },
-          data: {
-            premium: 1,
-            premiumExpiration: new Date(
-              subscriptionUpdated.data[0].current_period_end * 1000
-            )
-          }
-        })
+				await prisma.guild.update({
+					where: {
+						guildID: serverIdUpdated
+					},
+					data: {
+						premium: 1,
+						premiumExpiration: new Date(
+							subscriptionUpdated.data[0].current_period_end * 1000
+						)
+					}
+				})
 
-        break
+				break
 
-      // in the event of a subscription being deleted
-      case 'customer.subscription.deleted':
-        const subscriptionDataDeleted: Stripe.Subscription = event.data.object
-        const serverIdDeleted = subscriptionDataDeleted.metadata?.serverId
+			// in the event of a subscription being deleted
+			case 'customer.subscription.deleted':
+				const subscriptionDataDeleted: Stripe.Subscription = event.data.object
+				const serverIdDeleted = subscriptionDataDeleted.metadata?.serverId
 
-        if (!serverIdDeleted) {
-          console.error('One or more variables are undefined.')
-          return NextResponse.json(
-            { message: 'One or more variables are missing', status: 400 },
-            { status: 400 }
-          )
-        }
+				if (!serverIdDeleted) {
+					console.error('One or more variables are undefined.')
+					return NextResponse.json(
+						{ message: 'One or more variables are missing', status: 400 },
+						{ status: 400 }
+					)
+				}
 
-        try {
-          await prisma.guild.update({
-            where: {
-              guildID: serverIdDeleted
-            },
-            data: {
-              premium: 0,
-              premiumExpiration: null,
-              premiumUser: null
-            }
-          })
-        } catch (error) {
-          console.error(error)
-          return NextResponse.json(
-            {
-              message: 'An error occurred while updating the database.',
-              status: 500
-            },
-            { status: 500 }
-          )
-        }
+				try {
+					await prisma.guild.update({
+						where: {
+							guildID: serverIdDeleted
+						},
+						data: {
+							premium: 0,
+							premiumExpiration: null,
+							premiumUser: null
+						}
+					})
+				} catch (error) {
+					console.error(error)
+					return NextResponse.json(
+						{
+							message: 'An error occurred while updating the database.',
+							status: 500
+						},
+						{ status: 500 }
+					)
+				}
 
-        break
+				break
 
-      default:
-        console.log('Unhandled event type:', event.type)
-    }
+			default:
+				console.log('Unhandled event type:', event.type)
+		}
 
-    return NextResponse.json({ message: 'success', status: 'success' })
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { message: error.message, status: 500 },
-        { status: 500 }
-      )
-    } else {
-      return NextResponse.json(
-        { message: 'An unknown error occurred', status: 500 },
-        { status: 500 }
-      )
-    }
-  }
+		return NextResponse.json({ message: 'success', status: 'success' })
+	} catch (error) {
+		if (error instanceof Error) {
+			return NextResponse.json(
+				{ message: error.message, status: 500 },
+				{ status: 500 }
+			)
+		} else {
+			return NextResponse.json(
+				{ message: 'An unknown error occurred', status: 500 },
+				{ status: 500 }
+			)
+		}
+	}
 }
