496821ed4e1d140960ef06d1edf7d3875e2c37c9
Taqib <taqib.ibrahim@gmail.com>
2025-02-17 22:56:12 +0100

    feat: cooked og images for packs

--- app/api/packs/[id]/og/route.tsx
+++ app/api/packs/[id]/og/route.tsx
@@ -2,7 +2,34 @@
 import { ImageResponse } from 'next/og'
 import { prisma } from '@/lib/prisma'
 import validator from 'validator'
-import loadGoogleFont from '@/helpers/og/loadGoogleFont';
+import axios from 'axios'
+import sharp from 'sharp';
+
+async function loadGoogleFont(font: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}:opsz,wght@14..32,600..900&display=swap" rel="stylesheet`
+  const css = await (await fetch(url)).text()
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+
+  if (resource) {
+    const response = await fetch(resource[1])
+    if (response.status == 200) {
+      return await response.arrayBuffer()
+    }
+  }
+
+  throw new Error('failed to load font data')
+}
+
+async function getImageBase64(url: string) {
+  return axios.get<ArrayBuffer>(url, {
+    responseType: 'arraybuffer',
+  }).then(async (res) => {
+    const buffer = await sharp(res.data).toFormat('png').toBuffer()
+    return {
+      url: `data:${'image/png'};base64,${buffer.toString('base64')}`,
+    };
+  })
+}
 
 export async function GET(request: Request, props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -16,6 +43,7 @@ export async function GET(request: Request, props: { params: Promise<{ id: strin
       description: true,
       authorId: true,
       likes: true,
+      type: true,
     }
   })
 
@@ -42,6 +70,10 @@ export async function GET(request: Request, props: { params: Promise<{ id: strin
     select: { globalName: true, avatarUrl: true, profilePrivacy: true }
   })
 
+  const desc = "If you want to render the generated SVG to another image format such as PNG, it would be better to use base64 encoded image data (or buffer) directly as props.src so no extra I/O is needed in Satori If you want to render the generated SVG to another image format such as PNG, it would be better to use base64 encoded image data (or buffer) directly as props.src so no extra I/O is needed in Satori:"
+
+  const descriptionToShow = packData.description.length > 200 ? packData.description.slice(0, 200) + "..." : packData.description;
+
   if (!authorData) {
     return new ImageResponse(
       <div style={{
@@ -64,7 +96,6 @@ export async function GET(request: Request, props: { params: Promise<{ id: strin
     authorData.globalName = "Private"
     authorData.avatarUrl = "https://cdn.discordapp.com/embed/avatars/0.png"
   } else {
-    const getImageBase64 = (await import('@/helpers/og/getImageBase64')).default;
     const res = await getImageBase64(authorData.avatarUrl!)
     authorData.avatarUrl = res.url
   }
@@ -73,62 +104,72 @@ export async function GET(request: Request, props: { params: Promise<{ id: strin
   return new ImageResponse(
     (
       <div
-        style={{ fontFamily: 'Inter' }}
-        tw="relative flex items-center justify-center w-full h-full"
+        style={{
+          fontFamily: 'Inter',
+          backgroundImage: 'url(https://wouldyoubot.gg/Background.png)', // Local image reference
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+        tw="relative flex flex-col items-center justify-center w-full h-full p-10"
       >
-        {/* Background Image for the entire canvas */}
-        <div
-          tw="absolute inset-0 w-full h-full"
-          style={{
-            backgroundImage: 'url(https://wouldyoubot.gg/Background.png)', // Local image reference
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}
-        ></div>
-
-        {/* Author Section */}
-        <div tw="flex items-center bg-gray-800/80 px-4 py-2 rounded-lg">
-          <img
-            src={authorData?.avatarUrl!}
-            width={24}
-            height={24}
-            alt="Author avatar"
-            tw="w-8 h-8 rounded-full"
-          />
-          <span tw="text-gray-300 text-xl ml-3">{authorData?.globalName ?? "None"}</span>
-        </div>
-
-        {/* Content Section */}
-        <div tw="flex flex-col gap-6">
-          <h1 tw="text-6xl font-bold text-white">{packData.name}</h1>
-          <p tw="text-3xl text-gray-400 max-w-2xl leading-snug">
-            {packData.description}
-          </p>
-        </div>
-
-        {/* Stats Section */}
-        <div tw="flex gap-12 mt-auto">
-          <div tw="flex items-center gap-3">
-            <svg width="28" height="28" viewBox="0 0 20 20" fill="#EF4444">
-              <path
-                fillRule="evenodd"
-                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+        <div tw="w-full flex flex-col gap-8 mb-auto mt-24">
+          <div tw="flex items-center justify-between">
+            <div tw="flex items-center bg-gray-800/50 border border-gray-800 px-4 py-2 rounded-lg">
+              <img
+                src={authorData?.avatarUrl!}
+                width={24}
+                height={24}
+                alt="Author avatar"
+                tw="w-8 h-8 rounded-full"
               />
-            </svg>
-            <span tw="text-white text-2xl">84 Likes</span>
+              <span tw="text-gray-100 text-xl ml-3">{authorData?.globalName ?? "None"}</span>
+            </div>
+            {/* just to keep the pill from taking up the whole space */}
+            <div tw="flex flex-1"></div>
           </div>
 
-          <div tw="flex items-center gap-3">
-            <svg width="28" height="28" viewBox="0 0 20 20" fill="#3B82F6">
-              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-            </svg>
-            <span tw="text-white text-2xl">8383 Plays</span>
+          {/* Content Section */}
+          <div tw="flex flex-col">
+            <h1 tw="text-6xl font-bold text-white -mb-1">{packData.name}</h1>
+            <p tw="text-2xl text-white max-w-2xl leading-snug">
+              {descriptionToShow}
+            </p>
           </div>
         </div>
 
-        {/* Logo */}
-        <div tw="absolute flex bottom-16 right-16 w-20 h-20">
-          <span tw="w-full h-full rounded-xl bg-gradient-to-r from-red-500 to-blue-500"></span>
+        <div
+          tw="relative flex items-center justify-between w-full"
+        >
+          {/* Stats Section */}
+          <div tw="flex">
+            <div tw="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FFFFFF">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+              </svg>
+              <span tw="text-white text-2xl ml-3">{packData.type}</span>
+            </div>
+
+            <div tw="flex items-center ml-10">
+              <svg width="28" height="28" viewBox="0 0 20 20" fill="#EF4444">
+                <path
+                  fillRule="evenodd"
+                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                />
+              </svg>
+              <span tw="text-white text-2xl ml-3">{packData.likes.length} Likes</span>
+            </div>
+
+            <div tw="flex items-center ml-10">
+              <svg width="28" height="28" viewBox="0 0 20 20" fill="#3B82F6">
+                <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+              </svg>
+              <span tw="text-white text-2xl ml-3">8383 Plays</span>
+            </div>
+          </div>
+          {/* Logo */}
+          <div tw="flex w-16 h-16 ml-auto">
+            <img src="https://wouldyoubot.gg/Logo.svg" alt="Logo" tw="w-full h-full" />
+          </div>
         </div>
       </div>
     ),
