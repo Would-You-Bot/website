75601d3d1f35739dfc2edff18f995148c796391b
Dominik Koch <dominik@koch-bautechnik.de>
2025-03-04 00:36:05 +0100

    fix: update imprint

--- app/legal-de/page.tsx
+++ app/legal-de/page.tsx
@@ -35,6 +35,12 @@ export default function legalnoticede() {
       </h1>
       <p>Informationen gemäß § 5 TMG.</p>
       <div>
+        <h3 className="text-lg font-bold text-foreground">Einzelunternehmen</h3>
+        <p className="select-none">
+          Dominik Koch
+        </p>
+      </div>
+      <div>
         <h3 className="text-lg font-bold text-foreground">Kontakt</h3>
         <p className="select-none">
           Dominik Koch
