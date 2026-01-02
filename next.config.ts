b4637b373f6b34cd1cd7636b1e985f8eb553653b
Dominik K. <dominik@rivo.gg>
2026-01-02 23:29:29 +0100

    fix: remove startup fame

--- next.config.ts
+++ next.config.ts
@@ -96,12 +96,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**'
       },
-      {
-        protocol: 'https',
-        hostname: 'startupfa.me',
-        port: '',
-        pathname: '/**'
-      }
     ]
   }
 }
