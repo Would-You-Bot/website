b1ad06ead78b52eacf585201a7746afb48980457
Dominik K. <dominik@rivo.gg>
2026-05-17 09:59:58 +0200

    style(team): remove staff card hover effect

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -70,7 +70,7 @@ export default function Team() {
                   .map((staff) => (
                     <li
                       key={staff.name}
-                      className="flex basis-full transform cursor-default flex-col content-center rounded-2xl bg-background-light px-4 py-8 shadow-lg transition duration-300 ease-in-out hover:shadow-2xl sm:basis-[calc(50%-1.5rem)] sm:px-6 lg:basis-[calc(25%-2rem)] lg:px-8 lg:hover:-translate-y-2 xl:px-10"
+                      className="flex basis-full cursor-default flex-col content-center rounded-2xl bg-background-light px-4 py-8 shadow-lg sm:basis-[calc(50%-1.5rem)] sm:px-6 lg:basis-[calc(25%-2rem)] lg:px-8 xl:px-10"
                     >
                       <Image
                         className="mx-auto h-28 w-28 rounded-full md:h-24 md:w-24"
